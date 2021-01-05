b780ea06b5ace8adedf846e6c266b5fe5d0286fb
smb13 <smb111@mail.ru>
2021-01-05 20:51:09 +0300

    HW03 start

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -23,3 +23,15 @@ CREATE TABLE user_roles
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  user_id          INTEGER                 NOT NULL,
+  dateTime         TIMESTAMP DEFAULT now() NOT NULL,
+  description      VARCHAR                 NOT NULL,
+  calories         INTEGER                 NOT NULL,
+  CONSTRAINT user_id_dateTime_idx UNIQUE (user_id, dateTime),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX meals_unique_description_idx ON meals (description);
